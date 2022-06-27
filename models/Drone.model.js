7500001c44097a875044b86cb3d506886923594d
Irene Carrion Retamero <105889766+IreneCar@users.noreply.github.com>
2022-06-27 15:55:33 +0200

    model create

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,18 @@
 // Iteration #1
+const mongoose = require("mongoose");
+
+const dronSchema = new mongoose.Schema({
+  name: {
+    type: String
+  },
+  propellers: {
+    type: Number
+  },
+  maxSpeed: {
+    type: Number
+  }
+});
+
+const Dron = mongoose.model("Dron", dronSchema);
+//Pelicula -> peliculas
+module.exports = Dron;
